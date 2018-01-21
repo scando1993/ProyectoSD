dfcf560104049b748b17f79dd952e959e1ecbecc
J3S <johnnysuarez@outlook.com>
2018-01-21 18:21:26 -0500

    Agregado redis para la caché

---
+++ ServiceNews/src/main/java/com/microservices/news/ServiceNews.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.microservices.news;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author j3s
+ */
+@Service
+public class ServiceNews {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceNews.class);
+    
+    @Autowired
+    private NewsRepository repository;
+
+    public News getNews(int id) {
+        LOGGER.info("getNews called for id {}", id);
+        return repository.findByIdnews(id);
+    }
+
+    @Cacheable(value = "newscache", key = "#timestamp")
+    public List<News> getNewsTimestamp(long timestamp) {
+        LOGGER.info("getNewsTimestamp called for timestamp {}", timestamp);
+        return repository.findByTimestamp(timestamp);
+    }
+}
--- ServiceNews/src/main/java/com/microservices/news/ServiceNewsApp.java
+++ ServiceNews/src/main/java/com/microservices/news/ServiceNewsApp.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -19,6 +20,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 // Registro del servicio en Eureka
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCaching
 public class ServiceNewsApp {
     
     
